d029aad4805e4712275632b7fc61a53259f96bc5
Eugene <19798308+AwesomeObserver@users.noreply.github.com>
2018-10-17 11:57:19 +0300

    Upd

--- wsl/init.sh
+++ wsl/init.sh
@@ -10,6 +10,15 @@ sudo apt install zsh
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
 #Install Powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
