637d04027345087a92ff14a1af668ac21b1633db
Eugene <19798308+AwesomeObserver@users.noreply.github.com>
2018-10-21 17:23:27 +0300

    Fix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,24 +1,28 @@
-boot.initrd.luks.devices = [
-  {
-    name = "root";
-    device = "/dev/sda3";
-    preLVM = true;
-  }
-];
+{ config, pkgs, ... }:
 
-users.extraUsers.awesome_observer = {
-  name = "awesome_observer";
-  group = "users";
-  extraGroups = [
-    "wheel" "disk" "audio" "video"
-    "networkmanager" "systemd-journal"
+{
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/sda3";
+      preLVM = true;
+    }
   ];
-  createHome = true;
-  uid = 1000;
-  home = "/home/awesome_observer";
-  shell = "/run/current-system/sw/bin/bash";
-};
-
-boot.loader.grub.device = "/dev/sda";
-
-networking.wireless.enable = true;
+  
+  users.extraUsers.awesome_observer = {
+    name = "awesome_observer";
+    group = "users";
+    extraGroups = [
+      "wheel" "disk" "audio" "video"
+      "networkmanager" "systemd-journal"
+    ];
+    createHome = true;
+    uid = 1000;
+    home = "/home/awesome_observer";
+    shell = "/run/current-system/sw/bin/bash";
+  };
+  
+  boot.loader.grub.device = "/dev/sda";
+  
+  networking.wireless.enable = true;
+}
