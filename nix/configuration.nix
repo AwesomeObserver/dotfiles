5f8f8610b9cbacb2c49fc05ac305a6a01081b9ee
Eugene <19798308+AwesomeObserver@users.noreply.github.com>
2018-10-21 17:19:01 +0300

    Upd

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -6,6 +6,19 @@ boot.initrd.luks.devices = [
   }
 ];
 
+users.extraUsers.awesome_observer = {
+  name = "awesome_observer";
+  group = "users";
+  extraGroups = [
+    "wheel" "disk" "audio" "video"
+    "networkmanager" "systemd-journal"
+  ];
+  createHome = true;
+  uid = 1000;
+  home = "/home/awesome_observer";
+  shell = "/run/current-system/sw/bin/bash";
+};
+
 boot.loader.grub.device = "/dev/sda";
 
 networking.wireless.enable = true;
